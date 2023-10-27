b3929bff4d88eddada6ff25cb62ea6e051977756
Kanishtha Sahu <kanishtha3122001@gmail.com>
2023-10-27 13:39:19 +0530

    Adding the responsive UI assignment given by Aimed Labs

---
+++ responsive_ui/lib/constants.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+var myDefaultColor = Colors.white;
+var myGreenBox = Container(
+  height: 30,
+  width: 650,
+  color: Color.fromARGB(255, 172, 243, 175),
+);
+var myGreyBox = Container(
+  width: 300,
+  height: 30,
+  color: Colors.grey,
+);
